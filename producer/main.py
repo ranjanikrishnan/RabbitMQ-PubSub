9ab6f2bc27efc81b52cedcd51ddda861b3d50b0e
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-07-17 11:20:40 +0200

    main code for the producer

--- producer/main.py
+++ producer/main.py
@@ -1,7 +1,11 @@
-from users.parse_data import get_users
-from users.publish import publish
+import time
+
+from user.parse_data import get_users
+from user.publish import publish
 
 
-users_dict = get_users('producer/data/data_example.csv')
-print('users_dict: ', users_dict)
-publish(users_dict)
+time.sleep(20)
+users_list = get_users('./data/data_example.csv')
+print('users_list: ', users_list)
+for value in users_list:
+    publish(value)
